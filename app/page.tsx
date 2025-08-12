8ac8baea5e52749342e9a46309c62bd2f432282b
Azkhal20 <azkalzavier@gmail.com>
2025-08-13 00:28:05 +0700

    setup project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl text-center p-8">First Commit</h1>
+    </>
+  );
+}
